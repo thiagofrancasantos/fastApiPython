07f1022f711e8af7deb1d304aba6a742d96c1476
thiagofrancasantos <francasantos.t@gmail.com>
2025-09-24 12:50:31 -0400

    busca de dados e tratamento de erros

--- fastApii/main.py
+++ fastApii/main.py
@@ -20,8 +20,19 @@ cursos = {
     }
 }
 
-print('teste funcionando')
+@app.get('/cursos')
+async def get_cursos():
+    return cursos
+
+@app.get('/cursos/{curso_id}')
+async def get_curso(curso_id: int):
+    curso = cursos[curso_id]
+    curso.update({"id": curso_id})
+
+    return curso
+
+
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, debug=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
